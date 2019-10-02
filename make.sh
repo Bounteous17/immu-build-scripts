9ed72b5e60929ee1b1daa89ef667a820ea78df4a
Bounteous <bounteous17@protonmail.com>
2019-10-02 18:55:21 +0200

    migrate start.sh >> make.sh

--- make.sh
+++ make.sh
@@ -5,4 +5,17 @@ if [[ ${EUID} -ne 0 ]]; then
     exit 1
 fi
 
-./start.sh
+archiso_path=.archlive
+out=out
+
+echo "Removing old build files: ${archiso_path}, ${out}"
+rm -rf ${archiso_path} ${out}
+cp -r /usr/share/archiso/configs/releng/ ${archiso_path}
+cp -vfp ./build.sh ${archiso_path}
+
+if [ ! -d ${out} ]; then
+    mkdir -v ${out};
+fi
+
+cd ${archiso_path}
+./build.sh -v
